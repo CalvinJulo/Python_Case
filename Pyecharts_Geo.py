03a379450accc9b1e9042c9b09fec84ed1493f4f
Calvin Julo <zcglook@163.com>
2020-01-16 19:06:30 +0800

    Update Pyecharts_Geo.py

--- Pyecharts_Geo.py
+++ Pyecharts_Geo.py
@@ -6,7 +6,6 @@ import json
 html='''
 {"cmts":[{"approve":0,"approved":false,"assistAwardInfo":{"avatar":"","celebrityId":0,"celebrityName":"","rank":0,"title":""},"avatarurl":"https://img.meituan.net/avatar/2f6ce3e8ad6350a531aeada401601b0c21747.jpg","cityName":"呼和浩特","content":"完美完美完美","filmView":false,"gender":2,"id":1096231601,"isMajor":false,"juryLevel":0,"majorType":0,"movieId":1200486,"nick":"南楠19900520","nickName":"南楠19900520","oppose":0,"pro":false,"reply":0,"score":4.5,"spoiler":0,"startTime":"2020-01-14 15:25:12","supportComment":true,"supportLike":true,"sureViewed":1,"tagList":{"fixed":[{"id":1,"name":"好评"},{"id":4,"name":"购票"}]},"time":"2020-01-14 15:25","userId":94843213,"userLevel":2,"videoDuration":0,"vipType":0},{"approve":0,"approved":false,"assistAwardInfo":{"avatar":"","celebrityId":0,"celebrityName":"","rank":0,"title":""},"avatarurl":"https://img.meituan.net/maoyanuser/4be405f274fa8fca7358a9e579a8262411960.png","cityName":"沈阳","content":"很好看，值得推荐！","filmView":false,"id":1096224083,"isMajor":false,"juryLevel":0,"majorType":0,"movieId":1200486,"nick":"xhluan","nickName":"xhluan","oppose":0,"pro":false,"reply":0,"score":5.0,"spoiler":0,"startTime":"2020-01-14 14:59:02","supportComment":true,"supportLike":true,"sureViewed":1,"tagList":{"fixed":[{"id":1,"name":"好评"},{"id":4,"name":"购票"}]},"time":"2020-01-14 14:59","userId":60345158,"userLevel":1,"videoDuration":0,"vipType":0}],"total":689370}
 '''
-
 datas=json.loads(html,strict=False)['cmts']  #将类Json格式的字符串转化为Json格式
 
 comments=[]
